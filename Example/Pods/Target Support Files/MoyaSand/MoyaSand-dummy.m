6d63f540a929438091f4f65295f6158901ae24cc
Michael Sanderson <git@mikesand.com>
2017-02-09 13:22:43 -0500

    Pod install

---
+++ Example/Pods/Target Support Files/MoyaSand/MoyaSand-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_MoyaSand : NSObject
+@end
+@implementation PodsDummy_MoyaSand
+@end
---
+++ Example/Pods/Target Support Files/Pods-MoyaSand_Tests/Pods-MoyaSand_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MoyaSand_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_MoyaSand_Tests
+@end
